a14b1f7e95977beb6029fdec2e781cffa4c1b54f
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-04-15 11:34:59 -0400

    updating Flaxe

--- Nix/NewNixDots/Flakes/flake.nix
+++ Nix/NewNixDots/Flakes/flake.nix
@@ -3,17 +3,25 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ... }:
+  outputs = {self, nixpkgs, home-manager, ... }:
     let
+      system = "x86_64-linux";
       lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
         nixos = lib.nixosSystem {
-          system = "x86_64-linux";
+          inherit system;
           modules = [ ./configuration.nix ];
       };
     };
+      homeConfigurations = {
+        milktruck = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
   };
 }
--- Nix/NewNixDots/Flakes/home.nix
+++ Nix/NewNixDots/Flakes/home.nix
@@ -3,8 +3,8 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "testenv";
-  home.homeDirectory = "/home/testenv";
+  home.username = "milktruck";
+  home.homeDirectory = "/home/milktruck";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
