6bae666bb264f7ef92d0ca8341038733ee19d860
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-04-15 11:42:34 -0400

    commit

--- Nix/NewNixDots/Flakes/home.nix
+++ Nix/NewNixDots/Flakes/home.nix
@@ -18,27 +18,28 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    bash
+    zsh
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+
+    ".aliases" = {
+      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/alias";
+      };
+    
+    ".p10k.zsh" = {
+      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/.p10k.zsh";
+      executable = true;
+    };
+    
+    ".zshrc" = {
+      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/zshrc";
+      executable = true;
+    };
+
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
