c291ae968853ee657f28aed4ae3522920336b1f3
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-04-15 12:55:09 -0400

    fixing...

--- Nix/NewNixDots/Flakes/home.nix
+++ Nix/NewNixDots/Flakes/home.nix
@@ -9,30 +9,15 @@
     stateVersion = "23.11"; # Please read the comment before changing.
 
     sessionVariables = {	
+
     };
 
     packages = [
-
         kitty
         zsh
-
       ];
 
     file = {
-    
-        ".aliases" = {
-          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/alias";
-          };
-        
-        ".p10k.zsh" = {
-          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/.p10k.zsh";
-          executable = true;
-        };
-        
-        ".zshrc" = {
-          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/zshrc";
-          executable = true;
-        };
       };
     };
     programs.home-manager.enable = true;
