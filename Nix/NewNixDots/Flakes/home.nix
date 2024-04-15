a8e10e89f59d5826183ca3a148c16acd5fdc15d6
dthomsen116 <david.thomsen@mymail.champlain.edu>
2024-04-15 11:49:43 -0400

    commit

--- Nix/NewNixDots/Flakes/home.nix
+++ Nix/NewNixDots/Flakes/home.nix
@@ -3,70 +3,38 @@
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
-  home.username = "milktruck";
-  home.homeDirectory = "/home/milktruck";
+  home = {
+    name = "milktruck";
+    homeDirectory = "/home/milktruck";
+    stateVersion = "23.11"; # Please read the comment before changing.
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
+    sessionVariables = {	
+    };
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    bash
-    zsh
-  ];
+    packages = [
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
+        kitty
+        zsh
 
-    ".aliases" = {
-      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/alias";
+      ];
+
+    file = {
+    
+        ".aliases" = {
+          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/alias";
+          };
+        
+        ".p10k.zsh" = {
+          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/.p10k.zsh";
+          executable = true;
+        };
+        
+        ".zshrc" = {
+          source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/zshrc";
+          executable = true;
+        };
       };
-    
-    ".p10k.zsh" = {
-      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/.p10k.zsh";
-      executable = true;
     };
-    
-    ".zshrc" = {
-      source = "github:/dthomsen116/DotFiles/Nix/NewNixDots/zsh/zshrc";
-      executable = true;
-    };
-
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    programs.home-manager.enable = true;
   };
-
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/testenv/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    # EDITOR = "emacs";
-  };
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
 }
